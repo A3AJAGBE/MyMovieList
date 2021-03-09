b9cc90d272f97c1d7972c3d3a056d7d64c304b0d
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-09 09:14:06 +0000

    Add movies database

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    image_url = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'Movie Title: {self.title}'
+
+
+db.create_all()
 
 
 @app.route('/')
