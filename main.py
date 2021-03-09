173e14d2b07a6a18c7ad3e3534dc00373f256669
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-09 09:34:06 +0000

    Add movie form

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField, SubmitField
+from wtforms.validators import DataRequired, url
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies-collection.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 class Movies(db.Model):
@@ -26,14 +35,38 @@ class Movies(db.Model):
 db.create_all()
 
 
+class MovieForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    year = IntegerField('Year', validators=[DataRequired()])
+    desc = StringField('Summary', validators=[DataRequired()])
+    rating = FloatField('Rating', validators=[DataRequired()])
+    ranking = IntegerField('Ranking', validators=[DataRequired()])
+    review = StringField('Review', validators=[DataRequired()])
+    image = StringField('Image Url', validators=[DataRequired(), url()])
+    submit = SubmitField('Submit')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/add')
+@app.route('/add', methods=["GET", "POST"])
 def add():
-    return render_template('add.html')
+    form = MovieForm()
+    if request.method == 'POST':
+        new_movie = Movies(
+            title=request.form['title'],
+            year=request.form['year'],
+            desc=request.form['desc'],
+            rating=request.form['rating'],
+            ranking=request.form['ranking'],
+            review=request.form['review'],
+            image_url=request.form['image_url'])
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
 
 
 @app.route('/edit')
